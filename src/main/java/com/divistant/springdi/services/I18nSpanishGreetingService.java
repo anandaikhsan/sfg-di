76d2c32ce80c78dd88ddb276aa9d10a4b56d025c
Ananda Mukhammad Ikhsan <anchor040599@gmail.com>
2020-06-26 01:38:19 +0700

    Contoh penggunaan default profile pada Bean

--- src/main/java/com/divistant/springdi/services/I18nSpanishGreetingService.java
+++ src/main/java/com/divistant/springdi/services/I18nSpanishGreetingService.java
@@ -3,7 +3,13 @@ package com.divistant.springdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES") // Bean ini tersedia untuk profile 'EN'
+/**
+ * Bean ini tersedia untuk profile 'ES'.
+ * Profile 'default' menandakan jika tidak
+ * ada konfigurasi profile yang aktif, maka
+ * Bean ini yang akan dipilih.
+  */
+@Profile({"ES", "default"})
 @Service("i18nGreetingService") // Memberi nama(qualifier) pada Bean
 public class I18nSpanishGreetingService implements GreetingService {
     @Override
